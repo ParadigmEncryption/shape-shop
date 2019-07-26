54c3cdd7d9919573f5f02c9d8b4fc56d14e43130
Paradigm <ParadigmEncryption@gmail.com>
2019-07-25 21:07:27 -0500

    added homepage and shape components

---
+++ src/components/shape/shape.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './shape.styles.scss';
+
+const Shape = props => (
+  <div className='shape-container'>
+    <div className='shape'>
+      <div className='circle'>
+
+      </div>
+    </div>
+    <div className='shape-footer'>
+      <span>BASIC</span>
+    </div>
+  </div>
+);
+
+export default Shape;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Shape from '../../components/shape/shape.component';
+
+import './homepage.styles.scss';
+
+const Homepage = props => {
+  return (
+    <Shape />
+  );
+}
+
+export default Homepage;
